c24e2d1d655252274878d85c1694f8475007b69d
Dantess5 <115427225+Dantess5@users.noreply.github.com>
2023-01-15 01:38:45 +0300

    2.0

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,10 @@
 package koschei.models;
 
+
 public class Needle7 {
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
